82587c383d648008919df7f7985e8ad39b7e67c2
Edgar-Del <delfinotchissingui@gmail.com>
2025-02-19 14:01:58 +0100

    reestruturação do projecto adicionando o FastAPI do Python

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.api.endpoints import router
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
+
+app.include_router(router, prefix=settings.API_V1_STR)
