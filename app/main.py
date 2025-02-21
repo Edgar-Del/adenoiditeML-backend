b27595e0c3d65cf227b9539a4cc095a971556df3
Edgar-Del <delfinotchissingui@gmail.com>
2025-02-21 15:02:56 +0100

    actualização do Cors para requisições externo

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import router
 
 app = FastAPI(title="API ML Adenoidite", version="1.0")
 
-# Inclui os endpoints definidos no arquivo endpoints.py
+# Configurar CORS para permitir requisições do frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir qualquer origem (ou especifique a do frontend)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos HTTP (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # Permitir todos os headers
+)
+
 app.include_router(router, prefix="/api/v1")
 
 if __name__ == "__main__":
